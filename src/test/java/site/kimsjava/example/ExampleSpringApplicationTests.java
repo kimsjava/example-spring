6301aa2b27278c75e23a6843d0b2b32123a4495d
kimsjava <kimsjava@gmail.com>
2021-03-03 17:41:27 +0900

    first commit

---
+++ src/test/java/site/kimsjava/example/ExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kimsjava.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
